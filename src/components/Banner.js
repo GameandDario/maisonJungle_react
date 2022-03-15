bed9c7e63bd293e26c4b5579b499431cc11b008d
Stéphane <stephane.sokeo@gmail.com>
2022-03-15 22:21:50 +0100

    add recommandation in banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,10 +3,20 @@ import logo from "../assets/logo.png";
 
 function Banner() {
   const title = "La maison jungle";
+  const currentMonth = new Date().getMonth();
+
+  const isSpring = currentMonth >= 2 && currentMonth <= 5;
+  const recommandation = isSpring ? (
+    <div>C'est le printemps rempotez ! 🌱</div>
+  ) : (
+    <div>C'est pas le moment de rempoter</div>
+  );
+
   return (
     <div className="lmj-banner">
       <img className="lmj-logo" src={logo} alt="logo - La maison jungle" />
       <h1 className="lmj-title">{title}</h1>
+      {recommandation}
     </div>
   );
 }
